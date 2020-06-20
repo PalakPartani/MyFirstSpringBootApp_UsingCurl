586a841eff916691516eca3c1d11e6247705c3ac
PalakPartani <partanipalak16@gmail.com>
2020-06-20 02:06:06 -0700

    ADDED:feature to use PostMapping to display deatils

--- src/main/java/com/bl/demo/controller/HelloController.java
+++ src/main/java/com/bl/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.bl.demo.controller;
 
+import com.bl.demo.model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +19,11 @@ public class HelloController {
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + "!";
+    }
+
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + user.getLastName() + "!";
     }
 
 }
